3cc74456431ae812dd84ce95582a7faa98e7a85a
Prashanth8088 <goudaprashant2204@gmail.com>
2026-02-16 18:47:41 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { ENV } from './lib/env.js';
+
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    res.status(200).json({msg:"Success from api"})
+})
+
+
+app.listen(ENV.PORT,()=>{
+    console.log("Server Running On the PORT:",ENV.PORT);
+    
+})
